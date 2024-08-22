5b22a4811a98e4fed1d42a43d77595e88915a842
Poojith Malla <111541172+poojithmalla8@users.noreply.github.com>
2024-08-22 09:44:04 -0700

    Time: 85 ms (89.62%) | Memory: 16.5 MB (48.56%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,21 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+        colset=[set() for _ in range(9)]
+        rowset=[set() for _ in range(9)]
+        boxset=[set() for _ in range(9)]
+
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
-                    return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                num=board[r][c]
 
+                if num=='.':
+                    continue
+                
+                if num in rowset[r]:
+                    return False
+                rowset[r].add(num)
+
+                if num in colset[c]:
+                    return False
+                colset[c].add(num)
+
+                subgrid_index=(r//3)*3+(c//3)
+
+                if num in boxset[subgrid_index]:
+                    return False
+                boxset[subgrid_index].add(num)
         return True
